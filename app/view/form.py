642c085d7957ee0c2be3e5c4a8eb81c9af675eea
eduardomatoss <eduardo-matos004@hotmail.com>
2022-08-28 17:37:19 -0300

    feat: adding captcha

--- app/view/form.py
+++ app/view/form.py
@@ -1,7 +1,23 @@
 import streamlit as st
 
+from app.service.captcha import create_captcha
+
+image, captcha_text = create_captcha()
+
 
 def registration():
-    st.title("Bank XPTO")
+    st.title("Form Registration")
 
-    print("teitei")
+    with st.form("my_form"):
+        st.write("Registration")
+        st.text_input("Name")
+        st.text_input("Last Name")
+        st.image(image)
+        captcha_input = st.text_input("Captcha")
+
+        register = st.form_submit_button("Register")
+        if register:
+            if captcha_text == captcha_input:
+                st.success("Captcha Valid")
+            else:
+                st.error("Captcha Invalid")
